bf6901df189256ea025746169728d4b567d58756
AnandJoe <yueyi14@hotmail.com>
2022-09-16 17:20:25 +0800

    添加模型权重文件和数据集配置文件

---
+++ RP_YOLO/View/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RP_YOLO
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string ImagePath { get; private set; }
+
+        private void BtnLoadImageClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "images(*.png;*.jpg;*.bmp)|*.png;*.bmp;*jpg",
+                Title = "打开图片"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImagePath = openFileDialog.FileName;
+            }
+        }
+    }
+}
